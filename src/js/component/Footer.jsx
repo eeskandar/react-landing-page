2e359c9724ffccc776d4e615417c54c3ea1b510a
Iskandar <seik747@gmail.com>
2022-08-19 23:28:19 +0000

    Footer function

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Footer = () => {
+    return (
+      <footer className="bg-dark text-muted py-5">
+        <div className="container d-flex align-items-center justify-content-center">
+          <h5>Copyright © Your Site</h5>
+        </div>
+      </footer>
+    )
+  };
